4a69390c457630f75f9d2318f22162f5cdf38b35
John T. Wodder II <git@varonathe.org>
2022-11-29 19:55:48 -0500

    Wait longer and show more information

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use expectrl::{spawn, Error};
+use std::time::Duration;
 
 fn main() -> Result<(), Error> {
+    println!("Spawning ...");
     let mut p = spawn("cat")?;
+    p.set_expect_timeout(Some(Duration::from_secs(3)));
+    println!("Sending line ...");
     p.send_line("Hello World")?;
+    println!("Expecting line ...");
     p.expect("Hello World")?;
+    println!("OK!");
     Ok(())
 }
